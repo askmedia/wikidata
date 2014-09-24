9886a9e3778952cc992f6f151545f98897499250
Olivier Mourlevat <olivier.mourlevat@gmail.com>
2014-09-24 13:53:23 +0200

    legendScale

--- web/js/main.js
+++ web/js/main.js
@@ -1,12 +1,21 @@
-// ZOOM 
+"use strict";
 
 jQuery(document).ready(function() {
+
     var size, ratio, default_size;
+    var screenWidth = jQuery(window).width();
+    var screenHeight = jQuery(window).height();
+    if (screenWidth < 1280)
+        screenHeight -= 127;
+    size = Math.min(screenHeight, screenWidth);
+
+    /*
     default_size = 1200;
-    size = Math.min(jQuery(window).height(), jQuery(window).width());
     ratio = (size * 100) / default_size;
     ratio = 100 - ratio;
+
     console.log(ratio);
+    */
 
     // jQuery('.range').each(function(i) {
     //     jQuery(this).css({
@@ -19,6 +28,7 @@ jQuery(document).ready(function() {
     //     });
     // });
 
+    /*
     jQuery('.range-1').css({
         height: ((jQuery('.range-1').height() * ratio) / 100) + 15
     });
@@ -54,6 +64,8 @@ jQuery(document).ready(function() {
         'margin-left': -(((jQuery('.range-legend--4').width() * ratio) / 100) / 2),
         width: (jQuery('.range-legend--4').width() * ratio) / 100
     });
+*/
+
     jQuery('.wrapper__graph__body').css({
         height: size,
         width: size
@@ -70,6 +82,55 @@ jQuery(document).ready(function() {
             height: 300
         }
     });
+
+    setTimeout(function() {
+        var base_width = 1200;
+
+        var human_value = 2424305;
+        var human_width = 223;
+        var human_radius = human_width / 2;
+        var human_area = human_radius * Math.PI * Math.PI;
+
+        var legendRatio = human_value / human_area;
+
+        console.log('human_value', human_value);
+        console.log('human_area', human_area);
+        console.log('ratio : ', legendRatio);
+
+        var x = 100;
+
+        var legends = {
+            1: 47000,
+            2: 100000,
+            3: 1000000,
+            4: 2400000
+        };
+        console.log(legends);
+        for (var i in legends) {
+
+            var range_value = legends[i];
+            var range_area = range_value / legendRatio;
+            var range_radius = range_area / Math.PI / Math.PI;
+            var range_width = range_radius * 2;
+
+            var $img = jQuery(".sxy-zoom-bg");
+
+            var imgRatio = size / 1200;
+
+            jQuery('.range-' + i + ' span').text(range_value);
+
+            jQuery('.range-' + i).css({
+                height: range_width * imgRatio + 30
+            });
+            jQuery('.range-legend--' + i).css({
+                height: range_width * imgRatio,
+                width: range_width * imgRatio,
+                'margin-left': -range_width * imgRatio / 2,
+                'margin-top': range_width * imgRatio / 2
+            });
+        }
+
+    }, 1500);
 
     jQuery('.see-more').on('click', function() {
         var _this, _see_more_body;
@@ -92,6 +153,8 @@ jQuery(document).ready(function() {
             jQuery('.wrapper__graph').fadeIn();
         });
     });
+
+    jQuery('.bt-explore').click();
 
     var big_Img = "images/what_is_Wikipedia_about-8000.jpg";
     jQuery('<img src="' + big_Img + '">').load(function() {
